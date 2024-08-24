416bd549a81fbc1fca6ac071c369d32114c83091
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2024-08-24 01:47:12 -0300

    feat(api): - Adicionar endpoint para busca de usuários. - Novo endpoint GET /api/v1/jogadores que permite buscar novos jogadores que ja foram registrados de novos usuários no sistema. Inclui validação de dados e tratamento de erros. Closes #456

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtios/criar-jogador.dto';
 import { JogadoresService } from './jogadores.service';
 import { Jogador } from './interfaces/jogador.interface';
@@ -11,7 +11,7 @@ export class JogadoresController {
     async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
         await this.JogadoresService.criarAtualizarJogador(criarJogadorDto)
     }
-
+    @Get()
     async consultarJogadores(): Promise <Jogador[]> {
         return this.JogadoresService.consultarTodosJogadores();
     }
