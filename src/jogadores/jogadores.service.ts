5305cc7ab4bfe435b0ed1092f64c9d3d5c0d3179
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2024-09-13 23:58:13 -0300

    Fix(service):Retirada dos awats desnecessarios do codigo. Closes #4

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -10,26 +10,26 @@ export class JogadoresService {
     
     async criarAtualizarJogador(criarJogadorDto:CriarJogadorDto):Promise<void> {
         const { email } = criarJogadorDto;
-        const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email);
+        const jogadorEncontrado = this.jogadores.find(jogador => jogador.email === email);
         if ( jogadorEncontrado ) {
             return this.atualizar(jogadorEncontrado,criarJogadorDto);
         }else{
-            await this.criar(criarJogadorDto);
+             this.criar(criarJogadorDto);
         }
     }
 
     async consultarTodosJogadores(): Promise<Jogador[]> {
-        return  await this.jogadores;
+        return  this.jogadores;
     }
     
     async deletarJogadores(email): Promise<void>{
-        const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email);
+        const jogadorEncontrado =  this.jogadores.find(jogador => jogador.email === email);
         this.jogadores = this.jogadores.filter(jogadores => jogadores.email !== jogadorEncontrado.email)
         console.log(email);
     }
 
     async consultarJogadoresPeloEmail(email:string): Promise<Jogador> {
-        const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email);
+        const jogadorEncontrado =  this.jogadores.find(jogador => jogador.email === email);
         if(!jogadorEncontrado){
             throw new NotFoundException(`Jogador com e-mail ${email} nao encontrado!`);
         }
